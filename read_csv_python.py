f2886ff25d0073994f92141cd86d3f0ceefa8e16
unknown <igorbrandt95@hotmail.com>
2025-06-21 18:27:30 -0300

    :wrench: organizei arquivos .py separados para as diferentes formas de resolução

--- read_csv_python.py
+++ read_csv_python.py
@@ -1,5 +1,3 @@
-import pandas as pd
-# %%
 # 1. Dado um dicionário com os valores diários de uma cota de fundo, escreva um código em Python que calcule o retorno acumulado.
 
 # Com DICT
@@ -26,35 +24,3 @@ def calcular_retorno_total_acumulado_dict(cotacoes: dict) -> float:
     return retorno
 
 print(calcular_retorno_total_acumulado_dict(cotacoes_fundo_x))
-# %%
-
-# Com import csv
-import csv
-
-file_path = "data.csv"
-
-def csv_reader(file_to_read: str) -> list[dict]:
-    '''
-    Função que lê o csv
-    '''
-    with open(file_to_read, mode='r', encoding='utf-8') as open_file:
-        reader = csv.reader(open_file) # cria um iterador de linhas: um objeto que pode ser percorrido com for
-
-
-def calcular_retorno_total_acumulado_csv(x):
-    '''
-    Função que calcula o retorno total
-    '''
-# %%
-# Com import pandas
-import pandas as pd
-
-data = "data.csv"
-
-df = pd.DataFrame()
-print(df)
-
-
-
-
-# %%
